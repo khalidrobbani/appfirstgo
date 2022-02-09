fd43778db4d0bc7217ee4f524c63fd32c73e0038
khalidrobbani <ksrobbani@gmail.com>
2022-02-09 20:58:24 +0700

    menambah dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/khalidrobbani/appfirstgo
+
+go 1.17
+
+require github.com/khalidrobbani/firstgo v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	firstgo "github.com/khalidrobbani/firstgo"
+)
+
+func main() {
+	fmt.Println(firstgo.SayHello())
+}
